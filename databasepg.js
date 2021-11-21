4f656f1acc566b18c6fa59635e7d8126cc435af4
MariusGuju <marius.guju99@e-uvt.ro>
2021-11-22 00:04:08 +0200

    Update databasepg.js

--- databasepg.js
+++ databasepg.js
@@ -1,6 +1,5 @@
 const { Pool } = require('pg')
 
-
 const pool = new Pool({
     host: "localhost",
     user: "postgres",
@@ -9,12 +8,9 @@ const pool = new Pool({
     database: "remigoo"
 });
 
-
 pool.on('error', (err, client) => {
     console.error('Unexpected error on idle client', err)
     process.exit(-1)
   })
-
-
 
 module.exports = pool
